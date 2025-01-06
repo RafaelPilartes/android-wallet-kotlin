3625442552cb537140f030b991964fed0509594b
Rafael Pilartes <81381729+RafaelPilartes@users.noreply.github.com>
2025-01-06 12:41:29 +0200

    first commit

---
+++ app/src/main/java/com/example/wallet/data/entities/User.kt
@@ -0,0 +1,17 @@
+package com.example.wallet.data.entities
+
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "users",
+    indices = [Index(value = ["email"], unique = true)]
+)
+data class User(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0, // ID será gerado automaticamente
+    val name: String,
+    val email: String,
+    val password: String
+)
